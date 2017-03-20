040c8dc79e459e4405f153713030d43197b621d0
Mike Grabowski <grabbou@gmail.com>
2017-03-20 21:37:13 +0100

    Initial commit

---
+++ src/utils/findProvidesModule.js
@@ -0,0 +1,21 @@
+/**
+ * findProvidesModule
+ * 
+ * @description
+ * Recursively crawles given directories and builds a map
+ * of provided modules along with their absolute path.
+ */
+
+function findProvidesModule(directories, opts = {}) {
+  const options = Object.assign(opts, defaultOpts);
+
+  return {};
+}
+
+const defaultOpts = {
+  blacklist: [
+    /node_modules/
+  ],
+};
+
+module.exports = findProvidesModule;
