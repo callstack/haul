33fc87ec65afab7750ec1b852e16cecbcee7493e
Mike Grabowski <grabbou@gmail.com>
2017-03-21 17:01:07 +0100

    Working version

---
+++ src/utils/polyfillEnvironment.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2017-present, Callstack.
+ * All rights reserved.
+ * 
+ * polyfillEnvironment.js
+ */
+
+/**
+ * Polyfill `global` for React Native environment
+ */
+(function(global) {
+  global.global = global;
+  global.window = global;
+})(typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : this);
+
+/**
+ * Load polyfills set by packager by default
+ */
+require('react-native/packager/src/Resolver/polyfills/polyfills.js');
+require('react-native/packager/src/Resolver/polyfills/console.js');
+require('react-native/packager/src/Resolver/polyfills/error-guard.js');
+require('react-native/packager/src/Resolver/polyfills/Number.es6.js');
+require('react-native/packager/src/Resolver/polyfills/String.prototype.es6.js');
+require('react-native/packager/src/Resolver/polyfills/Array.prototype.es6.js');
+require('react-native/packager/src/Resolver/polyfills/Array.es6.js');
+require('react-native/packager/src/Resolver/polyfills/Object.es7.js');
+require('react-native/packager/src/Resolver/polyfills/babelHelpers.js');
+
