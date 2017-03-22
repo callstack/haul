9447857f5782882594c46d5d6552a6e2fd37ea32
Mike Grabowski <grabbou@gmail.com>
2017-03-22 09:31:53 +0100

    Tweaks

--- src/cli/start/index.js
+++ src/cli/start/index.js
@@ -7,14 +7,16 @@ const webpack = require('webpack');
 const Server = require('../../server');
 const path = require('path');
 
+/**
+ * Starts development server
+ */
 function start() {
   const config = require(path.join(process.cwd(), 'webpack.config.js'));
   const compiler = webpack(config);
 
   const app = new Server(compiler);
-
   app.listen(8081, "127.0.0.1", () => {
-	  console.log("Starting server on http://localhost:8081");
+    console.log("Starting server on http://localhost:8081");
   });
 }
 
