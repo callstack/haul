dd11fc7a2204d5d7d12d95256deb11225899e4f7
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-03-27 15:32:30 +0530

    Add asset resolver

---
+++ src/resolvers/assetResolver.js
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2017-present, Callstack.
+ * All rights reserved.
+ *
+ * @flow
+ */
+
+type Request = {
+  path: string,
+}
+
+type Options = {
+  test?: RegExp,
+  platform: 'ios' | 'android',
+}
+
+const exists = (fs, file) => new Promise(resolve => {
+  fs.stat(file, err => {
+    if (err) {
+      resolve(false);
+    } else {
+      resolve(true);
+    }
+  })
+});
+
+function AssetResolver(options: Options) {
+  const test = options.test || AssetResolver.test;
+  const platform = options.platform;
+
+  return function resolver() {
+    this.plugin('file', async (request: Request, callback: Function) => {
+      if (test.test(request.path)) {
+        const name = request.path.replace(/\.[^.]+$/, '');
+        const ext = request.path.split('.').pop();
+        const files = [
+          `${name}@1x.${platform}.${ext}`,
+          `${name}.${platform}.${ext}`,
+          `${name}@1x.native.${ext}`,
+          `${name}.native.${ext}`,
+          `${name}@1x.${ext}`,
+          request.path,
+        ];
+
+        for (const file of files) {
+          /* eslint-disable no-await-in-loop */
+          const result = await exists(this.fileSystem, file);
+
+          if (result) {
+            callback(null, Object.assign({}, request, {
+              path: file,
+              relativePath: request.relativePath && this.join(request.relativePath, file),
+              file: true,
+            }));
+            return;
+          }
+        }
+      }
+
+      callback();
+    });
+  };
+}
+
+AssetResolver.test = /\.(bmp|gif|jpg|jpeg|png)$/;
+
+module.exports = AssetResolver;
--- src/utils/makeReactNativeConfig.js
+++ src/utils/makeReactNativeConfig.js
@@ -10,6 +10,7 @@ const webpack = require('webpack');
 const HappyPack = require('happypack');
 const path = require('path');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const assetResolver = require('../resolvers/AssetResolver');
 const findProvidesModule = require('./findProvidesModule');
 
 const PLATFORMS = ['ios', 'android'];
@@ -47,11 +48,17 @@ const getDefaultConfig = ({ platform, cwd, dev }): WebpackConfig => ({
       {
         test: /\.js$/,
         exclude: /node_modules\/(?!react)/,
-        use: `${require.resolve('happypack/loader')}?id=babel`,
+        use: {
+          loader: require.resolve('happypack/loader'),
+          query: { id: 'babel' },
+        },
       },
       {
-        test: /\.(bmp|gif|jpg|jpeg|png)$/,
-        use: require.resolve('../loaders/assetLoader'),
+        test: assetResolver.test,
+        use: {
+          loader: require.resolve('../loaders/assetLoader'),
+          query: { platform },
+        },
       },
     ],
   },
@@ -85,10 +92,11 @@ const getDefaultConfig = ({ platform, cwd, dev }): WebpackConfig => ({
   ],
   // Default resolve
   resolve: {
+    plugins: [assetResolver({ platform })],
     alias: findProvidesModule([path.resolve(cwd, 'node_modules/react-native')]),
     mainFields: ['browser', 'main'],
     extensions: [`.${platform}.js`, '.native.js', '.js'],
-  },
+    },
 });
 
 /**
