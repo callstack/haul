711851098ddf29bb2957655705d6f04f52351a10
dratwas <drapich.piotr@gmail.com>
2017-03-28 21:44:05 +0200

    add flow to tests

--- src/server/middleware/__test__/statusPageMiddleware.test.js
+++ src/server/middleware/__test__/statusPageMiddleware.test.js
@@ -3,23 +3,25 @@
  * All rights reserved.
  *
  * statusPageMiddleware.test.js
+ *
+ * @flow
  */
 const statusPageMiddleware = require('../statusPageMiddleware');
 
 describe('status page middleware', () => {
   it('should respond with status:running', () => {
-    const url = '/status';
-    const end = jest.fn();
-    const next = jest.fn();
+    const url: string = '/status';
+    const end: Function = jest.fn();
+    const next: Function = jest.fn();
     statusPageMiddleware({ url }, { end }, next);
     expect(end).toHaveBeenCalledWith('packager-status:running');
     expect(next).not.toHaveBeenCalled();
   });
 
   it('should skip to next', () => {
-    const url = '/';
-    const end = jest.fn();
-    const next = jest.fn();
+    const url: string = '/';
+    const end: Function = jest.fn();
+    const next: Function = jest.fn();
     statusPageMiddleware({ url }, { end }, next);
     expect(end).not.toHaveBeenCalled();
     expect(next).toHaveBeenCalled();
