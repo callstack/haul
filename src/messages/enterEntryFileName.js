19a65334dc5a8d26cc4207015bb74679ed146433
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-03-30 16:39:58 +0530

    haul init command (#46)

---
+++ src/messages/enterEntryFileName.js
@@ -0,0 +1,8 @@
+/**
+ * Copyright 2017-present, Callstack.
+ * All rights reserved.
+ *
+ * @flow
+ */
+
+module.exports = () => `Enter the name of the entry file for the app`;
---
+++ src/messages/generatedConfig.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright 2017-present, Callstack.
+ * All rights reserved.
+ *
+ * @flow
+ */
+
+const chalk = require('chalk');
+
+module.exports = () => `Generated ${chalk.bold('webpack.haul.js')}`;
