30ea8af2011bb7258a4deb3adef90c9cb6da50e1
Mike Grabowski <grabbou@gmail.com>
2017-03-31 18:40:24 +0200

    Include polyfills with babel-preset-env (#64)
    * Initial commit
    
    * Comment

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,12 @@
 /**
  * Copyright 2017-present, Callstack.
  * All rights reserved.
- * 
- * @flow
  */
 
-require('babel-register')({
-  ignore: /node_modules(?!\/haul)/,
-  retainLines: true,
-  sourceMaps: 'inline',
-});
+/* eslint-disable */
+
+// babel-preset-env will transform the line below into
+// individual requires for babel-polyfill based on environment
+require('babel-polyfill');
 
 require('./cliEntry')(process.argv.slice(2));
