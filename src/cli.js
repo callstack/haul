87a9c1ee8e9f832132a68243631c624881a8002d
Mike Grabowski <grabbou@gmail.com>
2017-03-31 18:37:29 +0200

    Comment

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,11 @@
  * Copyright 2017-present, Callstack.
  * All rights reserved.
  */
+
 /* eslint-disable */
 
+// babel-preset-env will transform the line below into
+// individual requires for babel-polyfill based on environment
 require('babel-polyfill');
+
 require('./cliEntry')(process.argv.slice(2));
