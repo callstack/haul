a16847430560e17b37281d482b0858f56f1867d1
Mike Grabowski <grabbou@gmail.com>
2017-04-03 15:38:38 +0200

    Replace in-place

--- vendor/packager/haul-integrate.sh
+++ vendor/packager/haul-integrate.sh
@@ -3,18 +3,14 @@
 # React Native adds few scripts into Xcode build phase. Some of them
 # are defined internally inside React.xcodeproj which makes it impossible
 # to receive environmental variables set.
-#
-# To customise those scripts and make them run `Haul` instead of `packager`,
-# we copy our custom versions every time user hits run.
 
 THIS_DIR=$(dirname $0)
 
-PACKAGER_DIR="$(cd "${THIS_DIR}/../../../react-native/packager" && pwd)"
+SRC="$(cd "${THIS_DIR}/../../../react-native/packager" && pwd)"
+
+# Replace local-cli with Haul in `react-native-xcode.sh`
+sed -i -e 's/$REACT_NATIVE_DIR\/local-cli\/cli.js/.\/node_modules\/.bin\/haul' ${SRC}/react-native-xcode.sh
 
 # Copy files
-cp -f ${THIS_DIR}/packager.sh ${PACKAGER_DIR}/packager.sh
-cp -f ${THIS_DIR}/react-native-xcode.sh ${PACKAGER_DIR}/react-native-xcode.sh
-
-# Set executable permissions
-chmod 755 ${PACKAGER_DIR}/packager.sh
-chmod 755 ${PACKAGER_DIR}/react-native-xcode.sh
+cp -f ${THIS_DIR}/packager.sh ${SRC}/packager.sh
+chmod 755 ${SRC}/packager.sh
