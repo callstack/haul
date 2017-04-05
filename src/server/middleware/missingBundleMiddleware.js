4452a6e880986900bb206757cb6f6066385f407f
Mike Grabowski <grabbou@gmail.com>
2017-04-05 15:05:38 +0200

    Update

--- src/server/middleware/missingBundleMiddleware.js
+++ src/server/middleware/missingBundleMiddleware.js
@@ -2,34 +2,26 @@
  * Copyright 2017-present, Callstack.
  * All rights reserved.
  */
-const MultiCompiler = require('webpack/lib/MultiCompiler');
 const dedent = require('dedent');
 
-function missingBundleMiddleware(compiler) {
-  return (req, res, next) => {
-    // In multi compiler mode, all entry points should be available
-    if (compiler instanceof MultiCompiler) {
-      next();
-    }
+function missingBundleMiddleware(req, res, next) {
+  // eslint-disable-next-line no-unused-vars
+  const [_, platform] = req.path.match(/(?:\.(ios|android))?\.bundle/) || [];
 
-    // eslint-disable-next-line no-unused-vars
-    const [_, platform] = req.path.match(/(?:\.(ios|android))?\.bundle/) || [];
+  if (platform) {
+    res.status(500).end(
+      dedent`
+      Bundle couldn't be loaded.
 
-    if (platform) {
-      res.status(500).end(
-        dedent`
-        Bundle couldn't be loaded.
+      Try the following to fix the issue:
+      - Ensure that Haul is running with --platform all or --platform ${platform}
+      - Ensure that bundle has compiled successfuly by checking terminal window where Haul is running
+    `,
+    );
+    return;
+  }
 
-        Try the following to fix the issue:
-        - Ensure that Haul is running with --platform all or --platform ${platform}
-        - Ensure that bundle has compiled successfuly by checking terminal window where Haul is running
-      `,
-      );
-      return;
-    }
-
-    next();
-  };
+  next();
 }
 
 module.exports = missingBundleMiddleware;
