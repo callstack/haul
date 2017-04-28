fd2ea85fe26f34b44f4f735e248a06e1cf09a0c9
Steve Kellock <steve@kellock.ca>
2017-04-28 09:27:18 -0400

    Opens an editor when tapping a stack frame. (#139)
    * Opens an editor when tapping a stack frame.
    
    * Renames HAUL_* to REACT_* & fixes my flow fail.

---
+++ src/messages/editorNotConfigured.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2017-present, Callstack.
+ * All rights reserved.
+ *
+ * @flow
+ */
+
+const chalk = require('chalk');
+const dedent = require('dedent');
+
+module.exports = () => dedent`
+  Unable to open in editor. You can set environment variables to open your editor.
+
+  --- Easiest (auto-detect) ---
+
+    ${chalk.bold('export REACT_EDITOR=code')}
+    ${chalk.gray('or')}
+    ${chalk.bold('export REACT_EDITOR=/path/to/atom')}
+
+    ${chalk.gray('(sublime, atom, code, webstorm, phpstorm, idea14ce, vim, emacs, visualstudio)')}
+    ${chalk.gray('via https://github.com/lahmatiy/open-in-editor')}
+
+
+  --- Harder (you have a custom script or symlink) ---
+
+    ${chalk.bold('export REACT_EDITOR=vim')}
+    ${chalk.bold('export REACT_EDITOR_CMD=/usr/local/bin/nvim')}
+  
+    ${chalk.gray('You can use this if you have recognized editor, but in non-standard location.')}
+
+
+  --- Hardest (completely custom -- hold my beer) ---
+
+    ${chalk.bold('export REACT_EDITOR_CMD=/path/to/a/crazy/editor')}
+    ${chalk.bold('export REACT_EDITOR_PATTERN="-r -g {filename}:{line}:{column}"')}
+
+    ${chalk.gray('You can use this if your editor is unknown or you want to launch with different flags.')}
+
+`;
