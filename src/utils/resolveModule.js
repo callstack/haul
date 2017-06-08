335874e233be7b97921ee1febab7e439c3e30c9a
Krzysztof Borowy <krzysztoff.borowy@gmail.com>
2017-06-08 10:41:00 +0200

    Added module resolver

---
+++ src/utils/resolveModule.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright 2017-present, Callstack.
+ * All rights reserved.
+ *
+ * @flow
+ */
+
+const resolver = require('resolve');
+
+/**
+* Resolves the path to a given module
+* We point to 'package.json', then remove it to receive a path to the directory itself
+*/
+
+module.exports = (root, name) =>
+  resolver
+    .sync(`${name}/package.json`, { basedir: root })
+    .replace('/package.json', '');
