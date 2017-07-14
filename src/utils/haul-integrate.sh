fc13147ecc63ca7c74de07f9ba17d439169c4571
howardchi <howardchi@fougood.com>
2017-07-14 10:37:06 +0800

    test react-native/packager is exist or not, if not use react-native/scripts

--- src/utils/haul-integrate.sh
+++ src/utils/haul-integrate.sh
@@ -2,7 +2,13 @@
 
 THIS_DIR=$(dirname $0)
 
-SRC="$(cd "${THIS_DIR}/../../../react-native/packager" && pwd)"
+SCRIPT_SRC="${THIS_DIR}/../../../react-native/packager"
+if [ ! -d "${SCRIPT_SRC}" ]; then
+  # Check if react-native/packager is exist, for >= 0.46 RN version.
+  SCRIPT_SRC="${THIS_DIR}/../../../react-native/scripts"
+fi
+
+SRC="$(cd "${SCRIPT_SRC}" && pwd)"
 
 # Replace local-cli with Haul in `react-native-xcode.sh`
 sed -i -e 's|$REACT_NATIVE_DIR/local-cli/cli.js|./node_modules/.bin/haul|' ${SRC}/react-native-xcode.sh
