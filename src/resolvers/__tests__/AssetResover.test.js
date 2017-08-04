3b034a302ac99836551bd4d0d587f869b47264be
Michał Pierzchała <thymikee@gmail.com>
2017-08-04 15:34:07 +0200

    Moar tests for collect

--- src/resolvers/__tests__/AssetResover.test.js
+++ src/resolvers/__tests__/AssetResover.test.js
@@ -5,8 +5,18 @@
  * @flow
  */
 
+import fs from 'fs';
+import path from 'path';
 import webpack from 'webpack';
 import config from './fixtures/webpack.config.js';
+import AssetResolver from '../AssetResolver';
+
+const filesFromEntry = [
+  require.resolve('./fixtures/file.pdf'),
+  require.resolve('./fixtures/file@1x.jpeg'),
+  require.resolve('./fixtures/file@3x.png'),
+  require.resolve('./fixtures/file@2x.gif'),
+];
 
 test('Resolves to file@{number}x.{ext} if file.{ext} not present', done => {
   webpack(config, (err, stats) => {
@@ -19,17 +29,39 @@ test('Resolves to file@{number}x.{ext} if file.{ext} not present', done => {
     const assetPaths = stats.toJson().modules.map(module => module.identifier);
 
     try {
-      expect(assetPaths).toEqual(
-        expect.arrayContaining([
-          require.resolve('./fixtures/file.pdf'),
-          require.resolve('./fixtures/file@1x.jpeg'),
-          require.resolve('./fixtures/file@3x.png'),
-          require.resolve('./fixtures/file@2x.gif'),
-        ]),
-      );
+      expect(assetPaths).toEqual(expect.arrayContaining(filesFromEntry));
       done();
     } catch (error) {
       done.fail(error);
     }
   });
+});
+
+test('AssetResolver.collect returns empty object for empty list', () => {
+  const result = AssetResolver.collect([], {
+    name: 'filename',
+    type: 'jpeg',
+  });
+
+  expect(result).toEqual({});
+});
+
+test('AssetResolver.collect returns empty object when file not in the list', () => {
+  const result = AssetResolver.collect(['file.jpeg', 'filename.png'], {
+    name: 'filename',
+    type: 'jpeg',
+  });
+
+  expect(result).toEqual({});
+});
+
+test('AssetResolver.collect returns a map of paths to resolve', () => {
+  const files = fs.readdirSync(path.resolve(__dirname, './fixtures'));
+  const result = AssetResolver.collect(files, {
+    name: 'filename',
+    type: 'jpeg',
+    platform: 'ios',
+  });
+
+  expect(result).toMatchSnapshot();
 });
