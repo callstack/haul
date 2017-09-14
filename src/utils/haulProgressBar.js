5871ce9dd4f5b8fd358fdc5e48e2536d9d0b385a
Krzysztof Borowy <krzysztoff.borowy@gmail.com>
2017-09-14 15:41:07 +0200

    Make function more generic

--- src/utils/haulProgressBar.js
+++ src/utils/haulProgressBar.js
@@ -20,18 +20,41 @@ const barOptions = {
   width: 20,
   total: 100,
 };
+const labelOptions = {
+  android: {
+    color: 'green',
+    label: 'Android',
+  },
+  ios: {
+    color: 'cyan',
+    label: 'iOS',
+  },
+};
 
-function createBarFormat(plat: string) {
-  const platform =
-    plat === 'android'
-      ? `${chalk.bold.green('Android')}:`.padEnd(30)
-      : `${chalk.bold.cyan('iOS')}:`.padEnd(30);
+/*
+ * Create label based on passed platform
+ * We use `labelOptions` to customize know platforms
+ * If platform is not know, returns default styles
+ */
+function createLabel(platform: string) {
+  if (labelOptions[platform]) {
+    const { color, label } = labelOptions[platform];
+    return `${chalk.bold[color](label)}`.padEnd(30);
+  }
+  return `${chalk.bold.magenta(platform)}`.padEnd(30);
+}
+
+/*
+ * Create progress bar itself
+ */
+function createBarFormat(platform: string) {
+  const label = createLabel(platform);
 
   const leftBar = chalk.bold('[');
   const rightBar = chalk.bold(']');
   const percent = chalk.bold.blue(':percent');
 
-  return `${platform}${leftBar}:bar${rightBar} ${percent}`;
+  return `${label}${leftBar}:bar${rightBar} ${percent}`;
 }
 
 module.exports = function createProgressBar(newPlatform: string) {
--- src/utils/makeReactNativeConfig.js
+++ src/utils/makeReactNativeConfig.js
@@ -63,9 +63,9 @@ const getDefaultConfig = ({
     context: root,
     entry: [
       /**
-         * Polyfills we include for latest JS features
-         * It is also needed to setup the required environment
-         */
+       * Polyfills we include for latest JS features
+       * It is also needed to setup the required environment
+       */
       require.resolve('./polyfillEnvironment.js'),
     ],
     devtool: bundle ? 'source-map' : 'eval-source-map',
@@ -78,166 +78,145 @@ const getDefaultConfig = ({
     },
     module: {
       rules: [
-        /**
-         * Polyfills we include for latest JS features
-         * It is also needed to setup the required environment
-         */
-        require.resolve('./polyfillEnvironment.js'),
-      ],
-      devtool: bundle ? 'source-map' : 'eval-source-map',
-      output: {
-        path: path.join(root, 'dist'),
-        filename: `index.${platform}.bundle`,
-        publicPath: `http://localhost:${port}/`,
-      },
-      module: {
-        rules: [
+        { parser: { requireEnsure: false } },
+        {
+          test: /\.js$/,
           /**
-           * This is a non-standard feature
-           * And, it won't work coz we don't have DOM
+           * The React Native ecosystem publishes untranspiled modules
+           * We transpile modules prefixed with react for compatibility
            */
-          { parser: { requireEnsure: false } },
-          {
-            test: /\.js$/,
-            /**
-             * The React Native ecosystem publishes untranspiled modules
-             * We transpile modules prefixed with react for compatibility
-             */
-            exclude: /node_modules\/(?!react|@expo|pretty-format|haul)/,
-            use: {
-              loader: require.resolve('happypack/loader'),
-              query: { id: 'babel' },
-            },
+          exclude: /node_modules\/(?!react|@expo|pretty-format|haul)/,
+          use: {
+            loader: require.resolve('happypack/loader'),
+            query: { id: 'babel' },
           },
+        },
+        {
+          test: AssetResolver.test,
+          use: {
+            /**
+             * Asset loader enables asset management based on image scale
+             * This needs the AssetResolver plugin in resolver.plugins to work
+             */
+            loader: require.resolve('../loaders/assetLoader'),
+            query: { platform, root, bundle },
+          },
+        },
+      ],
+    },
+    plugins: [
+      /**
+       * MacOS has a case insensitive filesystem
+       * This is needed so we can error on incorrect case
+       */
+      new CaseSensitivePathsPlugin(),
+
+      new webpack.ProgressPlugin(perc => {
+        platformProgressBar(platform, perc);
+      }),
+
+      new webpack.DefinePlugin({
+        /**
+         * Various libraries like React rely on `process.env.NODE_ENV`
+         * to distinguish between production and development
+         */
+        'process.env': {
+          NODE_ENV: dev ? '"development"' : '"production"',
+          DEV_SERVER_ORIGIN: JSON.stringify(`http://localhost:${port}`),
+        },
+        __DEV__: dev,
+      }),
+      new webpack.LoaderOptionsPlugin({
+        minimize: !!minify,
+        debug: dev,
+      }),
+      new webpack.NamedModulesPlugin(),
+      /**
+       * By default, sourcemaps are only generated with *.js files
+       * We need to use the plugin to configure *.bundle to emit sourcemap
+       */
+      new webpack.SourceMapDevToolPlugin({
+        test: /\.(js|css|bundle)($|\?)/i,
+        filename: '[file].map',
+      }),
+      /**
+       * HappyPack runs transforms in parallel to improve build performance
+       */
+      new HappyPack({
+        id: 'babel',
+        loaders: [
           {
-            test: AssetResolver.test,
-            use: {
+            path: require.resolve('babel-loader'),
+            query: Object.assign({}, getBabelConfig(root), {
               /**
-               * Asset loader enables asset management based on image scale
-               * This needs the AssetResolver plugin in resolver.plugins to work
+               * This enables caching results in ./node_modules/.cache/babel-loader/
+               * to improve the rebuild speeds
+               * This is a feature of `babel-loader` and not babel
                */
-              loader: require.resolve('../loaders/assetLoader'),
-              query: { platform, root, bundle },
-            },
+              cacheDirectory: dev,
+            }),
           },
         ],
-      },
-      plugins: [
-        /**
-         * MacOS has a case insensitive filesystem
-         * This is needed so we can error on incorrect case
-         */
-        new CaseSensitivePathsPlugin(),
-        // new ProgressBarPlugin({
-        //   format: `[:bar] :percent`,
-        //   summary: false,
-        // }),
-        new webpack.ProgressPlugin(perc => {
-          platformProgressBar(platform, perc);
-        }),
-        new webpack.DefinePlugin({
-          /**
-           * Various libraries like React rely on `process.env.NODE_ENV`
-           * to distinguish between production and development
-           */
-          'process.env': {
-            NODE_ENV: dev ? '"development"' : '"production"',
-            DEV_SERVER_ORIGIN: JSON.stringify(`http://localhost:${port}`),
-          },
-          __DEV__: dev,
-        }),
-        new webpack.LoaderOptionsPlugin({
-          minimize: !!minify,
-          debug: dev,
-        }),
-        new webpack.NamedModulesPlugin(),
-        /**
-         * By default, sourcemaps are only generated with *.js files
-         * We need to use the plugin to configure *.bundle to emit sourcemap
-         */
-        new webpack.SourceMapDevToolPlugin({
-          test: /\.(js|css|bundle)($|\?)/i,
-          filename: '[file].map',
-        }),
-        /**
-         * HappyPack runs transforms in parallel to improve build performance
-         */
-        new HappyPack({
-          id: 'babel',
-          loaders: [
-            {
-              path: require.resolve('babel-loader'),
-              query: Object.assign({}, getBabelConfig(root), {
+        verbose: false,
+      }),
+    ]
+      .concat(
+        process.env.NODE_ENV === 'production'
+          ? []
+          : new webpack.HotModuleReplacementPlugin(),
+      )
+      .concat(
+        minify
+          ? [
+              new webpack.optimize.UglifyJsPlugin({
                 /**
-                 * This enables caching results in ./node_modules/.cache/babel-loader/
-                 * to improve the rebuild speeds
-                 * This is a feature of `babel-loader` and not babel
-                 */
-                cacheDirectory: dev,
-              }),
-            },
-          ],
-          verbose: false,
-        }),
-      ]
-        .concat(
-          process.env.NODE_ENV === 'production'
-            ? []
-            : new webpack.HotModuleReplacementPlugin(),
-        )
-        .concat(
-          minify
-            ? [
-                new webpack.optimize.UglifyJsPlugin({
-                  /**
                  * By default, uglify only minifies *.js files
                  * We need to use the plugin to configutr *.bundle to get minified
                  * Also disable IE8 support as we don't need it'
                  */
-                  test: /\.(js|bundle)($|\?)/i,
-                  sourceMap: true,
-                  compress: {
-                    screw_ie8: true,
-                    warnings: false,
-                  },
-                  mangle: {
-                    screw_ie8: true,
-                  },
-                  output: {
-                    comments: false,
-                    screw_ie8: true,
-                  },
-                }),
-              ]
-            : [],
-        ),
-      resolve: {
-        plugins: [
-          /**
-           * React Native uses a module system called Haste
-           * We don't support it, but need to provide a compatibility layer
-           */
-          new HasteResolver({
-            directories: [moduleResolve(root, 'react-native')],
-          }),
-          /**
-           * This is required by asset loader to resolve extra scales
-           * It will resolve assets like image@1x.png when image.png is not present
-           */
-          new AssetResolver({ platform }),
-        ],
+                test: /\.(js|bundle)($|\?)/i,
+                sourceMap: true,
+                compress: {
+                  screw_ie8: true,
+                  warnings: false,
+                },
+                mangle: {
+                  screw_ie8: true,
+                },
+                output: {
+                  comments: false,
+                  screw_ie8: true,
+                },
+              }),
+            ]
+          : [],
+      ),
+    resolve: {
+      plugins: [
         /**
-         * Match what React Native packager supports
-         * First entry takes precendece
+         * React Native uses a module system called Haste
+         * We don't support it, but need to provide a compatibility layer
          */
-        mainFields: ['react-native', 'browser', 'main'],
-        extensions: [`.${platform}.js`, '.native.js', '.js'],
-      },
+        new HasteResolver({
+          directories: [moduleResolve(root, 'react-native')],
+        }),
+        /**
+         * This is required by asset loader to resolve extra scales
+         * It will resolve assets like image@1x.png when image.png is not present
+         */
+        new AssetResolver({ platform }),
+      ],
       /**
-       * Set target to webworker as it's closer to RN environment than `web`.
+       * Match what React Native packager supports
+       * First entry takes precendece
        */
-      target: 'webworker',
+      mainFields: ['react-native', 'browser', 'main'],
+      extensions: [`.${platform}.js`, '.native.js', '.js'],
     },
+    /**
+     * Set target to webworker as it's closer to RN environment than `web`.
+     */
+    target: 'webworker',
   };
 };
 
