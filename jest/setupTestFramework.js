327cf11b0251c0a5d53d37fa2e89d241ea1c456e
Jakub Benes <jukben@gmail.com>
2017-11-09 09:11:53 +0100

    feat: better disable Chalk colors

--- jest/setupTestFramework.js
+++ jest/setupTestFramework.js
@@ -1,6 +1,9 @@
 /* global jest */
 
-jest.mock('chalk');
+/**
+ * Disable colours because of CI
+ */
+require('chalk').enabled = false;
 
 // Integration tests are quite slow to run on CI, feel free to adjust as needed
 jest.setTimeout(80000);
