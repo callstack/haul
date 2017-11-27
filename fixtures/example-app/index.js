638fde22156e141aaf6d6d8511b6ed118898f116
Michał Pierzchała <thymikee@gmail.com>
2017-11-27 16:27:25 +0100

    Simplify hot reload

--- fixtures/example-app/index.js
+++ fixtures/example-app/index.js
@@ -1,18 +1,5 @@
-import 'haul/hot/patch';
-import { makeHot, tryUpdateSelf, callOnce } from 'haul/hot';
+import 'haul/hot';
 import { AppRegistry } from 'react-native';
 import App from './App';
 
-tryUpdateSelf();
-
-callOnce(() => {
-  AppRegistry.registerComponent('Example', makeHot(() => App));
-});
-
-if (module.hot) {
-  module.hot.accept(() => {});
-  module.hot.accept('./App', () => {
-    clearCacheFor(require.resolve('./index'));
-    redraw(() => require('./index').default);
-  });
-}
+AppRegistry.registerComponent('Example', () => App);
