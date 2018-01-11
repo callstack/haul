3b6aaee1025ed4766e7f2acbb2f0741e97efb7dd
Paweł Trysła <pawel.trysla@gmail.com>
2018-01-11 12:41:45 +0100

    do not use foreign babel configs

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,46 @@
 #!/usr/bin/env node
 
-require('babel-register')({
-  ignore: /node_modules(?!\/haul)/,
-  retainLines: true,
-  sourceMaps: 'inline',
-});
+/**
+ * Babel config is stored in `package.json` so that
+ * Jest can read it.
+ */
+const babelConfig = require('../package.json').babel;
+
+function resolveModule(moduleId, type) {
+  return require.resolve(`babel-${type}-${moduleId}`);
+}
+
+function resolve(presetsOrPlugins, type) {
+  return presetsOrPlugins.map(item => {
+    return typeof item === 'string'
+      ? resolveModule(item, type)
+      : [resolveModule(item[0], type), item[1]];
+  });
+}
+
+/**
+ * To prevent `babel-register` from reading foreign Babel
+ * configs, `babelrc` has to be set to `false` and Haul's
+ * Babel config inlined into `babel-register`'s options.
+ * 
+ * Also presets and plugins need to be resolved to
+ * Haul's `node_modules` directory, otherwise Babel would
+ * search from them in project's `node_modules`.
+ */
+require('babel-register')(
+  Object.assign(
+    {
+      ignore: /node_modules(?!\/haul)/,
+      retainLines: true,
+      sourceMaps: 'inline',
+      babelrc: false,
+    },
+    babelConfig,
+    {
+      presets: resolve(babelConfig.presets, 'preset'),
+      plugins: resolve(babelConfig.plugins, 'plugin'),
+    }
+  )
+);
 
 require('../src/cli');
