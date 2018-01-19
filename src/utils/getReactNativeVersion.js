ceebd4cae45632d20d0fa318582b3de96ecb175b
Dave Irvine <dave@daveirvine.me>
2018-01-19 14:05:12 +0000

    Disable flow checks for require()

--- src/utils/getReactNativeVersion.js
+++ src/utils/getReactNativeVersion.js
@@ -8,6 +8,8 @@ const path = require('path');
 
 module.exports = function getReactNativeVersion(cwd: string) {
   try {
+    // We deliberately want to require a dynamic string, so we have to disable flow.
+    // $FlowFixMe
     const pak = require(path.join(
       cwd,
       'node_modules/react-native/package.json'
