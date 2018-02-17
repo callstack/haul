368042ec29ddcdf76289227864ae75e9004987b9
Paweł Trysła <pawel.trysla@gmail.com>
2018-02-17 18:25:12 +0100

    remove clear from runAdbReverse

--- src/server/util/runAdbReverse.js
+++ src/server/util/runAdbReverse.js
@@ -6,7 +6,6 @@
  */
 
 const { exec } = require('child_process');
-const clear = require('clear');
 
 const logger = require('../../logger');
 const messages = require('../../messages');
@@ -14,7 +13,6 @@ const messages = require('../../messages');
 module.exports = function runAdbReverse(port: number) {
   const command = `adb reverse tcp:${port} tcp:${port}`;
 
-  clear();
   exec(command, error => {
     if (error) {
       // Get just the error message
