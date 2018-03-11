166aea3a7890e45e0fda4e7c76a36a2d393f0206
Michał Pierzchała <thymikee@gmail.com>
2018-03-11 19:05:02 +0100

    Fix tests to cleanup after themselves

--- integration_tests/__tests__/init.test.js
+++ integration_tests/__tests__/init.test.js
@@ -16,11 +16,13 @@ const TEST_PROJECT_DIR = path.resolve(
   '../../fixtures/react-native-clean'
 );
 const CONFIG_FILE_PATH = path.resolve(TEST_PROJECT_DIR, 'webpack.haul.js');
+const GRADLE_PATH = path.resolve(TEST_PROJECT_DIR, 'android/app/build.gradle');
 const ENTER_KEY = '\x0d';
 
 beforeAll(() => run('yarn --mutex network', TEST_PROJECT_DIR));
 afterEach(() => rimraf.sync(CONFIG_FILE_PATH));
 beforeEach(() => rimraf.sync(CONFIG_FILE_PATH));
+afterAll(() => run(`git checkout ${GRADLE_PATH}`));
 
 test('init command on react-native project', done => {
   const haul = runHaul(TEST_PROJECT_DIR, ['init']);
