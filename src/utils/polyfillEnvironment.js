68859d6601813cabbf60284db6e0e6cf013c1858
Jakub Benes <jukben@gmail.com>
2018-04-21 12:19:17 +0200

    feat: better polyfills

--- src/utils/polyfillEnvironment.js
+++ src/utils/polyfillEnvironment.js
@@ -8,33 +8,6 @@
  */
 import { NativeModules } from 'react-native';
 
-/* eslint-disable import/no-extraneous-dependencies */
-require('../../vendor/polyfills/console.js')(global);
-require('../../vendor/polyfills/error-guard.js');
-require('../../vendor/polyfills/Number.es6.js');
-require('../../vendor/polyfills/String.prototype.es6.js');
-require('../../vendor/polyfills/Array.prototype.es6.js');
-require('../../vendor/polyfills/Array.es6.js');
-require('../../vendor/polyfills/Object.es6.js');
-require('../../vendor/polyfills/Object.es7.js');
-require('../../vendor/polyfills/babelHelpers.js');
-
-// HACK:
-//   This is horrible.  I know.  But this hack seems to be needed due to the way
-//   React Native lazy evaluates `fetch` within `InitializeCore`.  This was fixed
-//   in 34-ish, but seems to be back again.  I hope I'm wrong because I lost sleep
-//   on this one.
-//
-//   Without this in place, global.fetch will be undefined and cause the symbolicate
-//   check to fail.  This must be something that the packager is doing that haul isn't.
-//   I also so people complaining about this in Jest as well.
-//
-if (!global.self) {
-  global.self = global; /* eslint-disable-line */
-}
-
-require('react-native/Libraries/Core/InitializeCore');
-
 require('../hot/client/importScriptsPolyfill');
 
 if (process.env.NODE_ENV !== 'production') {
