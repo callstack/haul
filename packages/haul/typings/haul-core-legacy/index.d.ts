4d951df106194ac03f5f4b2b5ffe389428979a75
Paweł Trysła <pawel.trysla@gmail.com>
2019-04-25 15:54:52 +0200

    add typings to VCS

---
+++ packages/haul/typings/haul-core-legacy/index.d.ts
@@ -0,0 +1,100 @@
+declare module 'haul-core-legacy/build/commands/init' {
+  const legacyCommand: {
+    action: Function;
+  };
+  export default legacyCommand;
+}
+
+declare module 'haul-core-legacy/build/commands/reload' {
+  const legacyCommand: {
+    action: Function;
+  };
+  export default legacyCommand;
+}
+
+declare module 'haul-core-legacy/build/commands/start' {
+  type StartCommandOptions = {
+    port: number;
+    no_interactive?: boolean;
+    config: string;
+    assetsDest?: string;
+    dev: boolean;
+    minify: boolean;
+    eager?: boolean | string[];
+    hotReloading: boolean;
+  };
+
+  const legacyCommand: {
+    action: (options: StartCommandOptions) => Promise<void>;
+  };
+  export default legacyCommand;
+}
+
+declare module 'haul-core-legacy/build/constants' {
+  export const DEFAULT_PORT: number;
+  export const DEFAULT_CONFIG_FILENAME: string;
+  export const INTERACTIVE_MODE_DEFAULT: boolean;
+}
+
+declare module 'haul-core-legacy/build/utils/getEntryFiles' {
+  export default function getEntryFiles(entry: string[]): string;
+}
+
+declare module 'haul-core-legacy/build/utils/getConfig' {
+  import webpack from 'webpack';
+
+  export default function getConfig(...args: any[]): webpack.Configuration;
+}
+
+declare module 'haul-core-legacy/build/utils/getWebpackConfigPath' {
+  export default function getWebpackConfigPath(
+    dir: string,
+    userConfig?: string
+  ): string;
+}
+
+declare module 'haul-core-legacy' {
+  type EnvOptions = ConfigOptions & {
+    platform: string;
+  };
+  type WebpackConfigFactory =
+    | ((opts: EnvOptions) => WebpackConfig)
+    | WebpackConfig;
+  type WebpackEntry = string | Array<string> | Object;
+  type ConfigOptions = {
+    root: string;
+    assetsDest: string;
+    dev: boolean;
+    minify?: boolean;
+    bundle?: boolean;
+    port?: number;
+    providesModuleNodeModules?: (
+      | string
+      | { name: string; directory: string })[];
+    hasteOptions?: any;
+    initializeCoreLocation?: string;
+    disableHotReloading?: boolean;
+  };
+
+  type WebpackConfig = {
+    entry: WebpackEntry;
+    output: {
+      path: string;
+      filename: string;
+    };
+    name?: string;
+    plugins: Function[];
+    context: string;
+    optimization: {
+      minimize: boolean;
+      minimizer: Function[];
+      namedModules: boolean;
+      concatenateModules: boolean;
+    };
+    stats: string;
+  };
+
+  export function createWebpackConfig(
+    configBuilder: WebpackConfigFactory
+  ): (options: EnvOptions) => Object;
+}
---
+++ packages/haul/typings/simple-progress-webpack-plugin/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'simple-progress-webpack-plugin' {
+  export default class SimpleProgressWebpackPlugin {
+    constructor({ format }: { format: string });
+  }
+}
