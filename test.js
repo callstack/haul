70d9fe39f8782cf644954a5d608630e4ee51a806
Paweł Trysła <zamotany@users.noreply.github.com>
2019-05-08 17:33:31 +0200

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-/*!*************************************************************************************************!*\
-  !*** ./packages/haul-ram-bundle-webpack-plugin/src/__tests__/__fixtures__/nestedCrashFn.esm.js ***!
-  \*************************************************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports) {
-
-Object.defineProperty(exports,"__esModule",{value:true});exports.default=crashNested;function crashNested(){var a=1;throw new Error('Nested test error');}
-
-/***/ })
