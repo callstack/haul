a60a300de379ec35b0ffe60e6f351a4edcac9afb
Liron Erman <lironer@soluto.com>
2019-05-13 10:57:37 +0300

    Run yarn lint —fix

--- packages/haul-core/src/constants.ts
+++ packages/haul-core/src/constants.ts
@@ -3,4 +3,7 @@ import { join } from 'path';
 export const INTERACTIVE_MODE_DEFAULT = true;
 export const DEFAULT_CONFIG_FILENAME = 'haul.config.js';
 export const DEFAULT_PORT = 8081;
-export const ASSET_LOADER_PATH = join(__dirname, './webpack/loaders/assetLoader');
+export const ASSET_LOADER_PATH = join(
+  __dirname,
+  './webpack/loaders/assetLoader'
+);
