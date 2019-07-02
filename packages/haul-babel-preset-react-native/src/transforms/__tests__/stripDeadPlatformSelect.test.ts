1dfd1fc72e6a8a189787d758efaf4d529b8df005
Paweł Trysła <pawel.trysla@gmail.com>
2019-07-02 17:02:36 +0200

    move fixtures to test cases

--- packages/haul-babel-preset-react-native/src/transforms/__tests__/stripDeadPlatformSelect.test.ts
+++ packages/haul-babel-preset-react-native/src/transforms/__tests__/stripDeadPlatformSelect.test.ts
@@ -1,57 +1,24 @@
-import stripDeadPlatformSelect from '../stripDeadPlatformSelect';
 import { transformSync } from '@babel/core';
-
-const fixture1 = `
-const value = Platform.select({
-  android: 'android',
-  ios: () => {},
-  default: { test: true },
-  web: 'what?'
-});
-`;
-
-const fixture2 = `
-const defaults = { default: 'default' };
-const value = Platform.select({
-  ios: () => {},
-  ...defaults,
-});
-`;
-
-const fixture3 = `
-const value = Platform.select({
-  android: () => {},
-});
-`;
-
-const fixture4 = `
-const value = Platform.select({
-  ios: false,
-  ['andr' + 'oid']: () => {},
-});
-`;
-
-const fixture5 = `
-const value = Platform.select({
-  default: 'default',
-  ['andr' + 'oid']() {}
-});
-`;
-
-const fixture6 = `
-const value = Platform.select({
-  ios: false,
-  android() {}
-});
-`;
+import dedent from 'dedent';
+import stripDeadPlatformSelect from '../stripDeadPlatformSelect';
 
 describe('stripDeadPlatformSelect transform', () => {
   it('should replace Platform.select with value for default case if there is no matching platform', () => {
-    const { code } = transformSync(fixture1, {
-      plugins: [[stripDeadPlatformSelect, { platform: 'custom' }]],
-      babelrc: false,
-      configFile: false,
-    }) || { code: '' };
+    const { code } = transformSync(
+      dedent`
+      const value = Platform.select({
+        android: 'android',
+        ios: () => {},
+        default: { test: true },
+        web: 'what?'
+      });
+      `,
+      {
+        plugins: [[stripDeadPlatformSelect, { platform: 'custom' }]],
+        babelrc: false,
+        configFile: false,
+      }
+    ) || { code: '' };
 
     expect(code).toMatch(/const value = {\s*test: true\s*}/);
     expect(code).not.toMatch('Platform.select');
@@ -59,11 +26,20 @@ describe('stripDeadPlatformSelect transform', () => {
   });
 
   it('should replace Platform.select with value for matching platform', () => {
-    const { code } = transformSync(fixture2, {
-      plugins: [[stripDeadPlatformSelect, { platform: 'ios' }]],
-      babelrc: false,
-      configFile: false,
-    }) || { code: '' };
+    const { code } = transformSync(
+      dedent`
+      const defaults = { default: 'default' };
+      const value = Platform.select({
+        ios: () => {},
+        ...defaults,
+      });
+      `,
+      {
+        plugins: [[stripDeadPlatformSelect, { platform: 'ios' }]],
+        babelrc: false,
+        configFile: false,
+      }
+    ) || { code: '' };
 
     expect(code).toMatch('const value = () => {};');
     expect(code).not.toMatch('Platform.select');
@@ -71,11 +47,20 @@ describe('stripDeadPlatformSelect transform', () => {
   });
 
   it('should remove non-matching platforms but leave Platform.select', () => {
-    const { code } = transformSync(fixture2, {
-      plugins: [[stripDeadPlatformSelect, { platform: 'android' }]],
-      babelrc: false,
-      configFile: false,
-    }) || { code: '' };
+    const { code } = transformSync(
+      dedent`
+      const defaults = { default: 'default' };
+      const value = Platform.select({
+        ios: () => {},
+        ...defaults,
+      });
+      `,
+      {
+        plugins: [[stripDeadPlatformSelect, { platform: 'android' }]],
+        babelrc: false,
+        configFile: false,
+      }
+    ) || { code: '' };
 
     expect(code).toMatch('...defaults');
     expect(code).toMatch('Platform.select');
@@ -84,21 +69,36 @@ describe('stripDeadPlatformSelect transform', () => {
   });
 
   it('should replace Platform.select with undefined if no cases are matching', () => {
-    const { code } = transformSync(fixture3, {
-      plugins: [[stripDeadPlatformSelect, { platform: 'ios' }]],
-      babelrc: false,
-      configFile: false,
-    }) || { code: '' };
+    const { code } = transformSync(
+      dedent`
+      const value = Platform.select({
+        android: () => {},
+      });
+      `,
+      {
+        plugins: [[stripDeadPlatformSelect, { platform: 'ios' }]],
+        babelrc: false,
+        configFile: false,
+      }
+    ) || { code: '' };
 
     expect(code).toEqual('const value = undefined;');
   });
 
   it('should remove non-matching platforms but leave Platform.select with unknown cases 1', () => {
-    const { code } = transformSync(fixture4, {
-      plugins: [[stripDeadPlatformSelect, { platform: 'android' }]],
-      babelrc: false,
-      configFile: false,
-    }) || { code: '' };
+    const { code } = transformSync(
+      dedent`
+      const value = Platform.select({
+        ios: false,
+        ['andr' + 'oid']: () => {},
+      });
+      `,
+      {
+        plugins: [[stripDeadPlatformSelect, { platform: 'android' }]],
+        babelrc: false,
+        configFile: false,
+      }
+    ) || { code: '' };
 
     expect(code).toMatch("['andr' + 'oid']: () => {}");
     expect(code).toMatch('Platform.select');
@@ -107,11 +107,19 @@ describe('stripDeadPlatformSelect transform', () => {
   });
 
   it('should remove non-matching platforms but leave Platform.select with unknown cases and default case', () => {
-    const { code } = transformSync(fixture5, {
-      plugins: [[stripDeadPlatformSelect, { platform: 'android' }]],
-      babelrc: false,
-      configFile: false,
-    }) || { code: '' };
+    const { code } = transformSync(
+      dedent`
+      const value = Platform.select({
+        default: 'default',
+        ['andr' + 'oid']() {}
+      });
+      `,
+      {
+        plugins: [[stripDeadPlatformSelect, { platform: 'android' }]],
+        babelrc: false,
+        configFile: false,
+      }
+    ) || { code: '' };
 
     expect(code).toMatch("['andr' + 'oid']() {}");
     expect(code).toMatch('Platform.select');
@@ -120,11 +128,19 @@ describe('stripDeadPlatformSelect transform', () => {
   });
 
   it('should remove non-matching platforms but leave Platform.select with method', () => {
-    const { code } = transformSync(fixture6, {
-      plugins: [[stripDeadPlatformSelect, { platform: 'android' }]],
-      babelrc: false,
-      configFile: false,
-    }) || { code: '' };
+    const { code } = transformSync(
+      dedent`
+      const value = Platform.select({
+        ios: false,
+        android() {}
+      });
+      `,
+      {
+        plugins: [[stripDeadPlatformSelect, { platform: 'android' }]],
+        babelrc: false,
+        configFile: false,
+      }
+    ) || { code: '' };
 
     expect(code).toMatch('android() {}');
     expect(code).toMatch('Platform.select');
