6fb37eb664d86ff4213a8f1a6bb699536f093a18
Paweł Trysła <zamotany@users.noreply.github.com>
2019-07-16 18:19:21 +0200

    chore: add multi-bundle test suite (#594)

---
+++ fixtures/react_native_with_haul_0_60x_multibundle/haul.config.js
@@ -0,0 +1,30 @@
+import { withPolyfills, makeConfig } from "@haul-bundler/preset-0.59";
+
+export default makeConfig({
+  bundles: {
+    index: {
+      entry: withPolyfills('./src/host.js'),
+      dependsOn: ['base_dll'],
+    },
+    base_dll: {
+      entry: withPolyfills([
+        'react',
+        'react-native',
+        'react-navigation',
+        './src/BundleRegistry.js'
+      ]),
+      dll: true,
+      type: 'indexed-ram-bundle',
+    },
+    app0: {
+      entry: './src/app0',
+      dependsOn: ['base_dll'],
+      app: true,
+    },
+    app1: {
+      entry: './src/app1',
+      dependsOn: ['base_dll'],
+      app: true,
+    }
+  },
+});
