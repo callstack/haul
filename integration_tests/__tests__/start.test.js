94753bbbee2742c138d8ebca339459e534a15d48
Paweł Trysła <pawel.trysla@gmail.com>
2019-07-16 12:26:38 +0200

    refactor start integration test

--- integration_tests/__tests__/start.test.js
+++ integration_tests/__tests__/start.test.js
@@ -1,32 +1,27 @@
 /**
  * Copyright 2017-present, Callstack.
  * All rights reserved.
- *
- * @flow
  */
 
-const { cleanup } = require('../utils');
-const path = require('path');
-const os = require('os');
-const fetch = require('node-fetch');
-const stripAnsi = require('strip-ansi');
-const { spawn } = require('child_process');
+import path from 'path';
+import os from 'os';
+import fetch from 'node-fetch';
+import stripAnsi from 'strip-ansi';
+import { cleanup } from '../utils';
+import { runHaul } from '../runHaul';
 
 const TEMP_DIR = path.join(os.tmpdir(), 'haul-start-');
 const TEST_PROJECT_DIR = path.resolve(
   __dirname,
   '../../fixtures/react_native_with_haul'
 );
-const BIN = path.join(__dirname, '../runStart.js');
 
 describe('packager server', () => {
   let server;
+  let disposed = false;
+
   beforeAll(done => {
-    server = spawn('node', [BIN, TEST_PROJECT_DIR], {
-      env: process.env,
-      stdio: 'pipe',
-      cwd: TEST_PROJECT_DIR,
-    });
+    server = runHaul(TEST_PROJECT_DIR, ['start']);
 
     server.stderr.on('data', data => {
       done.fail(data.toString());
@@ -34,8 +29,9 @@ describe('packager server', () => {
 
     server.stdout.on('data', data => {
       const message = stripAnsi(data.toString()).trim();
-
-      if (message.match(/RUNNING/g)) {
+      if (message.match(/error/g) && !disposed) {
+        done.fail(message);
+      } else {
         done();
       }
     });
@@ -43,14 +39,15 @@ describe('packager server', () => {
 
   afterAll(() => {
     cleanup(TEMP_DIR);
+    disposed = true;
     server.kill();
   });
 
-  it.skip('compile bundle for iOS platform', () => testPlatform('ios'));
+  it('compile bundle for iOS platform', () => testPlatform('ios'));
 
-  it.skip('compile bundle for Android platform', () => testPlatform('android'));
+  it('compile bundle for Android platform', () => testPlatform('android'));
 
-  it.skip('compile bundle for both platforms', () =>
+  it('compile bundle for both platforms', () =>
     Promise.all([testPlatform('ios'), testPlatform('android')]));
 });
 
