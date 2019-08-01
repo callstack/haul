1c70e25bc76e3c47b8dd3416b7e365159e0a18eb
Tommy Nguyen <tn0502@gmail.com>
2019-08-01 14:51:23 +0200

    Put additional plugins behind an option

--- packages/haul-babel-preset-react-native/src/index.ts
+++ packages/haul-babel-preset-react-native/src/index.ts
@@ -1,19 +1,23 @@
 const defaultPlugins = [
-  [require('@babel/plugin-proposal-class-properties'), {loose: true}],
+  [require('@babel/plugin-proposal-class-properties'), { loose: true }],
   [require('@babel/plugin-proposal-optional-catch-binding')],
   [require('@babel/plugin-syntax-dynamic-import')],
   [require('@babel/plugin-syntax-export-default-from')],
-  [require('@babel/plugin-transform-classes')],
   [require('@babel/plugin-transform-react-jsx')],
-  [require('@babel/plugin-transform-shorthand-properties')],
   [require('@babel/plugin-transform-sticky-regex')],
-  [require('@babel/plugin-transform-template-literals'), {loose: true}],
   [require('@babel/plugin-transform-unicode-regex')],
   // For some reason native async/await don't behave correctly
   // on RN 0.59 on both platforms, so we need to transpile it
   // to native Promises.
   [require('./transforms/superMemberArrowFunction').default],
   [require('@babel/plugin-transform-async-to-generator')],
+];
+
+// Additional plugins for Hermes because it doesn't support ES6 yet
+const hermesPlugins = [
+  [require('@babel/plugin-transform-classes')],
+  [require('@babel/plugin-transform-shorthand-properties')],
+  [require('@babel/plugin-transform-template-literals'), { loose: true }],
 ];
 
 function isTypeScriptSource(fileName: string) {
@@ -24,7 +28,7 @@ function isTSXSource(fileName: string) {
   return !!fileName && fileName.endsWith('.tsx');
 }
 
-export default function getHaulBabelPreset() {
+export default function getHaulBabelPreset(options: { hermes: boolean }) {
   return {
     compact: false,
     overrides: [
@@ -33,16 +37,18 @@ export default function getHaulBabelPreset() {
         plugins: [require('@babel/plugin-transform-flow-strip-types')],
       },
       {
-        plugins: defaultPlugins.concat(
-          process.env.HAUL_PLATFORM
+        plugins: [
+          ...defaultPlugins,
+          ...(options.hermes ? hermesPlugins : []),
+          ...(process.env.HAUL_PLATFORM
             ? [
                 [
                   require('./transforms/stripDeadPlatformSelect'),
                   { platform: process.env.HAUL_PLATFORM },
                 ],
               ]
-            : []
-        ),
+            : []),
+        ],
       },
       {
         test: /node_modules\/react-native/,
