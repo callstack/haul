15361ade8e732215b84ae03af12230a193e61749
Paweł Trysła <pawel.trysla@gmail.com>
2019-09-04 11:03:39 +0200

    tweak monorepo fixture

--- fixtures/monorepo/app0/haul.config.js
+++ fixtures/monorepo/app0/haul.config.js
@@ -1,21 +1,8 @@
 import { withPolyfills, makeConfig } from '../../../packages/haul-preset-0.60';
 import makeBaseDllConfig from 'base-dll/makeConfig';
-import { join } from 'path';
 
 export default makeConfig({
   bundles: {
-    index:  ({ platform, dev }) => {
-      const basePath = join(
-        __dirname,
-        `node_modules/host/dist/${platform}/${dev ? 'dev' : 'prod'}`
-      );
-      const filename = `index${
-        platform === 'ios' ? '.jsbundle' : '.android.bundle'
-      }`;
-      return {
-        bundlePath: join(basePath, filename),
-      };
-    },
     base_dll: makeBaseDllConfig(),
     app0: {
       entry: './App',
--- fixtures/monorepo/host/haul.config.js
+++ fixtures/monorepo/host/haul.config.js
@@ -1,5 +1,6 @@
 import { withPolyfills, makeConfig } from '../../../packages/haul-preset-0.60';
 import makeBaseDllConfig from 'base-dll/makeConfig';
+import { join } from 'path';
 
 export default makeConfig({
   bundles: {
@@ -7,6 +8,19 @@ export default makeConfig({
       entry: withPolyfills('./host.js'),
       dependsOn: ['base_dll'],
     },
-    base_dll: makeBaseDllConfig(),
+    base_dll: makeBaseDllConfig(true), // copy bundle files
+    app0:  ({ platform, dev }) => {
+      const basePath = join(
+        __dirname,
+        `node_modules/app0/dist/${platform}/${dev ? 'dev' : 'prod'}`
+      );
+      const filename = `app0${
+        platform === 'ios' ? '.jsbundle' : '.android.bundle'
+      }`;
+      return {
+        bundlePath: join(basePath, filename),
+        copyBundle: true,
+      };
+    },
   },
 });
