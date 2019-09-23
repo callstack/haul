989ded6826c54f46c1b440f2ac58f82dbcfa1f9d
Paweł Trysła <pawel.trysla@gmail.com>
2019-09-23 13:35:35 +0200

    switch to network socket for fork - worker comms

--- packages/haul-core-legacy/src/compiler/createWebSocketServer.js
+++ packages/haul-core-legacy/src/compiler/createWebSocketServer.js
@@ -5,39 +5,14 @@
  * @flow
  */
 
-const xpipe = require('xpipe');
+const getPort = require('get-port');
 const http = require('http');
 const { Server } = require('ws');
-const fs = require('fs');
-
-function getSocketAddress() {
-  let address = null;
-  let triesLeft = 20;
-  while (!address && triesLeft > 0) {
-    triesLeft--;
-    address = xpipe.eq(
-      `/tmp/HAUL_SOCKET_${Math.floor(
-        (Date.now() * Math.random()) / 10000000
-      )}_.socket`
-    );
-    if (fs.existsSync(address)) {
-      address = null;
-    }
-  }
-
-  if (!address) {
-    throw new Error('Could not find any free socket address');
-  }
-
-  return address;
-}
 
 /**
  * Create WebSocket server using HTTP server over Unix socket.
  */
-module.exports = function createWebSocketServer() {
-  const socketAddress = getSocketAddress();
-
+module.exports = async function createWebSocketServer() {
   const httpServer = http.createServer();
   const webSocketServer = new Server({
     server: httpServer,
@@ -45,7 +20,8 @@ module.exports = function createWebSocketServer() {
     maxPayload: 200 * 1024 * 1024,
   });
 
-  httpServer.listen(socketAddress);
+  const port = await getPort();
+  httpServer.listen(port);
 
   return webSocketServer;
 };
