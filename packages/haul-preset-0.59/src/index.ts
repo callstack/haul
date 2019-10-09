371db9d0004cc39a2877a96ae62d594850fa1d2d
Paweł Trysła <pawel.trysla@gmail.com>
2019-10-09 19:38:20 +0200

    move polyfills outside of src dir

--- packages/haul-preset-0.59/src/index.ts
+++ packages/haul-preset-0.59/src/index.ts
@@ -4,14 +4,14 @@ import getDefaultConfig from './defaultConfig';
 // JS polyfills for JSC
 // Temporary backport of https://github.com/facebook/react-native/blob/v0.55.3/rn-get-polyfills.js
 const polyfills = [
-  require.resolve('./vendor/polyfills/Object.es6.js'),
-  require.resolve('./vendor/polyfills/console.js'),
-  require.resolve('./vendor/polyfills/error-guard.js'),
-  require.resolve('./vendor/polyfills/Number.es6.js'),
-  require.resolve('./vendor/polyfills/String.prototype.es6.js'),
-  require.resolve('./vendor/polyfills/Array.prototype.es6.js'),
-  require.resolve('./vendor/polyfills/Array.es6.js'),
-  require.resolve('./vendor/polyfills/Object.es7.js'),
+  require.resolve('../vendor/polyfills/Object.es6.js'),
+  require.resolve('../vendor/polyfills/console.js'),
+  require.resolve('../vendor/polyfills/error-guard.js'),
+  require.resolve('../vendor/polyfills/Number.es6.js'),
+  require.resolve('../vendor/polyfills/String.prototype.es6.js'),
+  require.resolve('../vendor/polyfills/Array.prototype.es6.js'),
+  require.resolve('../vendor/polyfills/Array.es6.js'),
+  require.resolve('../vendor/polyfills/Object.es7.js'),
 ];
 
 export const withPolyfills = withPolyfillsFactory(polyfills);
