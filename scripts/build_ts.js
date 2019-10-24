993bd96a311df9399468d0225f0af0bbbd76c7ea
Paweł Trysła <pawel.trysla@gmail.com>
2019-10-24 13:48:25 +0200

    fix lint

--- scripts/build_ts.js
+++ scripts/build_ts.js
@@ -8,8 +8,6 @@ const packages = glob.sync('packages/*/tsconfig.json', {
   cwd: CWD,
 });
 
-execa.sync('yarn', ['clean'], { cwd: CWD, stdio: 'inherit' });
-
 execa.sync('yarn', ['tsc', '-b', ...packages, ...process.argv.slice(2)], {
   cwd: CWD,
   stdio: 'inherit',
