9ab66f5bddeae3c3114c1d43a802f4608d772d5d
Matt Hargett <plaztiksyke@gmail.com>
2019-11-20 21:30:58 -0800

    New fixture with yarn workspaces and correct hoisting.

---
+++ fixtures/monorepo-yarn-workspace/packages/app0/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'react-native',
+  setupFiles: [require.resolve('react-native/jest/setup')],
+  setupFilesAfterEnv: [require.resolve('jest-enzyme/lib')],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|react-navigation|@react-navigation|@react-native-community)',
+  ],
+  testMatch: ['<rootDir>/**/__tests__/**/*[.-_](spec|test).[tj]s?(x)'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+    '.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|dds|ogg|json.resource)$':
+      require.resolve('react-native/jest/assetFileTransformer.js'),
+  },
+};
