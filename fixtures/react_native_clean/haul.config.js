87d6ebb832f937fb23d19b8c1b9d89cae59a40c7
Paweł Trysła <zamotany.oss@gmail.com>
2020-01-09 17:11:02 +0100

    fix Hapi dependencies

--- fixtures/react_native_clean/haul.config.js
+++
@@ -1,9 +0,0 @@
-import { withPolyfills, makeConfig } from "@haul-bundler/preset-0.59";
-
-export default makeConfig({
-  bundles: {
-    index: {
-      entry: withPolyfills('./index'),
-    },
-  },
-});
