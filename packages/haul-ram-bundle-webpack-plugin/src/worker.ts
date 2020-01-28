79873bda84cf61d1137060fe59868dbf6e751544
NoemiRozpara <nrozpara@gmail.com>
2020-01-28 12:35:07 +0100

    fix(wip): import outside module in jest

--- packages/haul-ram-bundle-webpack-plugin/src/worker.ts
+++ packages/haul-ram-bundle-webpack-plugin/src/worker.ts
@@ -1,14 +1,30 @@
-import terser from 'terser';
+const terser = require('terser');
 
 type Code =
   | string
   | string[]
   | {
       [file: string]: string;
-    }
-  | terser.AST_Node;
+    };
 
-type Options = terser.MinifyOptions | undefined;
+type Options =
+  | {
+      compress?: boolean | object;
+      ecma?: any;
+      ie8?: boolean;
+      keep_classnames?: boolean | RegExp;
+      keep_fnames?: boolean | RegExp;
+      mangle?: boolean | object;
+      module?: boolean;
+      nameCache?: object;
+      output?: any;
+      parse?: any;
+      safari10?: boolean;
+      sourceMap?: boolean | object;
+      toplevel?: boolean;
+      warnings?: boolean | 'verbose';
+    }
+  | undefined;
 
 export const minify = async (code: Code, minifyOptions: Options) =>
   terser.minify(code, minifyOptions);
