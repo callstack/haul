fc9bddd4376902474a9e8d5a75677e48cbf5c85e
Brian Zhao <rezha@microsoft.com>
2020-05-10 20:03:24 -0700

    Check proper appium startup

--- fixtures/react_native_windows_e2e_app/__tests__/appium.test.js
+++ fixtures/react_native_windows_e2e_app/__tests__/appium.test.js
@@ -11,11 +11,52 @@ const driver = wd.promiseChainRemote('localhost', PORT);
 let appiumServer = { cancel() {} };
 let appiumLog = "";
 
-beforeAll(async (done) => {
-  appiumServer = execa.command('yarn run appium');
-  appiumServer.stdout.on('data', (data) => { appiumLog += data; });
-  await new Promise(resolve => setTimeout(resolve, 4000));
+// beforeAll(async (done) => {
+//   appiumServer = execa.command('yarn run appium');
+//   appiumServer.stdout.on('data', (data) => { appiumLog += data; });
+//   const t = new Date().getTime();
+//   await new Promise(resolve => setTimeout(resolve, 4000));
+//   console.log(new Date().getTime() - t);
 
+//   await driver.init({ ...config, app: 'Root' });
+//   await driver.sleep(2000);
+
+//   console.log('Getting RNWTestApp native window handle');
+//   const testAppWindow = await driver.elementByName('RNWTestApp');
+//   const testAppWindowHandle = await testAppWindow.getAttribute('NativeWindowHandle');
+//   const testAppHandle = parseInt(testAppWindowHandle, 10).toString(16);
+
+//   console.log('Initializing new session with top level window', testAppHandle);
+//   await driver.init({
+//     ...config,
+//     appTopLevelWindow: testAppHandle.toLowerCase(),
+//   });
+//   await driver.sleep(2000);
+//   done();
+// });
+// 
+// afterAll(async () => {
+//   console.log('Appium log:');
+//   console.log(appiumLog);
+//   await driver.quit();
+//   appiumServer.cancel();
+// });
+
+async function getByTestId(testId) {
+  const element = await driver.waitForElementByAccessibilityId(testId);
+  console.log('Found element', testId);
+  return element;
+}
+
+test('RNWTestApp renders correctly', async () => {
+  // Setup
+  appiumServer = execa.command('yarn run appium');
+  appiumServer.stdout.on('data', (data) => { console.log(`--> Appium: ${data}`); appiumLog += data.toString('utf8'); });
+  const t = new Date().getTime();
+  await new Promise(resolve => setTimeout(resolve, 4000));
+  console.log(new Date().getTime() - t);
+
+  console.log('test start');
   await driver.init({ ...config, app: 'Root' });
   await driver.sleep(2000);
 
@@ -30,23 +71,9 @@ beforeAll(async (done) => {
     appTopLevelWindow: testAppHandle.toLowerCase(),
   });
   await driver.sleep(2000);
-  done();
-});
 
-afterAll(async () => {
-  console.log('Appium log:');
-  console.log(appiumLog);
-  await driver.quit();
-  appiumServer.cancel();
-});
-
-async function getByTestId(testId) {
-  const element = await driver.waitForElementByAccessibilityId(testId);
-  console.log('Found element', testId);
-  return element;
-}
-
-test('RNWTestApp renders correctly', async () => {
+  // Test
+  console.log('test start');
   (await getByTestId('navigateToInitial')).click();
   await driver.sleep(1000);
 
@@ -64,4 +91,10 @@ test('RNWTestApp renders correctly', async () => {
   expect(await (await getByTestId('app1LoadTime')).text()).toMatch(/Load time: ([1-9]|[0-9]{2}) ms/);
 
   expect(await getByTestId('films')).toBeDefined();
-});
+
+  // Teardown
+  console.log('Appium log:');
+  console.log(appiumLog);
+  await driver.quit();
+  appiumServer.cancel();
+}, 120000);
